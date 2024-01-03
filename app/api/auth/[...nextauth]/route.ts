557239673fbbee3213bea1dd1ae44b709dcd5eab
kartik <kartikdhumal24@gmail.com>
2024-01-03 20:03:16 +0530

    import and export change

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt"
 import NextAuth, { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import GithubProvider from "next-auth/providers/github"
+// import GithubProvider from "next-auth/providers/github"
 import prisma from '../../../lib/prismadb'
 
 const authOptions: AuthOptions = {
