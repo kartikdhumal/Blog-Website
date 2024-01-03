6330013421a60c8cc4712482f3ed3048f44be8f6
kartik <kartikdhumal24@gmail.com>
2024-01-03 21:55:34 +0530

    showblog page.tsx write Anonymous

--- app/showblog/[blogId]/page.tsx
+++ app/showblog/[blogId]/page.tsx
@@ -3,35 +3,31 @@ import getBlogsById from "@/app/actions/getBlogsById";
 import SingleBlog from "@/components/blog/SingleBlog";
 import ShowBlog from "@/components/blog/ShowBlog";
 
-
 interface IParams {
-    blogId:string,
+    blogId: string,
 }
 
-export default async function page({params}:{params:IParams}) {
-    const blog  = await getBlogsById(params)
+export default async function page({ params }: { params: IParams }) {
+    const blog = await getBlogsById(params);
     const currentUser = await getCurrentUser();
     console.log(currentUser);
-    const date = blog?.createdAt
-    const date2 = new Date(date ?? 2023).toDateString()
+    const date = blog?.createdAt;
+    const date2 = new Date(date ?? 2023).toDateString();
 
- 
-  return (
-
-
-    <div className="">
-        <div>
-        <ShowBlog
-          name={blog?.name}
-          description={blog?.description}
-          blogId={blog?.id}
-          imageSrc={blog?.imageSrc}
-          createdAt={blog?.createdAt}
-          currentUserId={currentUser?.id}
-          userMade = {blog?.user.id}
-          nameUser= {blog?.user.name}
-        />
-      </div>
-    </div>
-  )
+    return (
+        <div className="">
+            <div>
+                <ShowBlog
+                    name={blog?.name}
+                    description={blog?.description}
+                    blogId={blog?.id}
+                    imageSrc={blog?.imageSrc}
+                    createdAt={blog?.createdAt}
+                    currentUserId={currentUser?.id}
+                    userMade={blog?.user.id}
+                    nameUser={blog?.user.name ?? "Anonymous"}
+                />
+            </div>
+        </div>
+    );
 }
