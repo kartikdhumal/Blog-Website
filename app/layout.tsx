b2bcc0925a554bade7d5cf344cd8704d4415045d
kartik <kartikdhumal24@gmail.com>
2024-01-04 22:18:18 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import Navbar from '@/components/navbar/Navbar'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import getCurrentUser from './actions/getCurrentUser'
-import { TProvider } from '@/providers/toast-provider'
-import getBlogs from './actions/getBlogs'
-import { HomeProps } from '@/utils/mytypes'
+import React from 'react';
+import Navbar from '@/components/navbar/Navbar';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import getCurrentUser from './actions/getCurrentUser';
+import { TProvider } from '@/providers/toast-provider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'DKTales',
   description: "Kartik Dhumal's blog app",
-}
+};
 
 export default async function RootLayout({
   children,
-}: HomeProps & { children: React.ReactNode }) {
-  const currentUser = await getCurrentUser()
+}: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
 
   return (
     <html lang="en">
@@ -26,5 +25,5 @@ export default async function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
