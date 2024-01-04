3b8ab9730283367ffdb27fa780cfad32f805cb43
kartik <kartikdhumal24@gmail.com>
2024-01-04 19:39:07 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import getBlogs, { IBlogParams } from "./actions/getBlogs";
 import SingleBlog from "@/components/blog/SingleBlog";
 import getCurrentUser from "./actions/getCurrentUser";
@@ -10,7 +9,7 @@ export interface HomeProps {
 export default async function Home({ blogParams }: HomeProps) {
   const currentUser = await getCurrentUser();
   const blogs = await getBlogs(blogParams);
-  
+
   return (
     <main className="bg-gradient-to-t from-blue-500 via-blue-600 to-blue-700 flex min-h-screen flex-col items-center justify-between gap-4 p-4">
       {blogs.length > 0 ? (
